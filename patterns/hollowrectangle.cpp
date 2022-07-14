b2aae6adb9806d41ca8129e1464b47e86ec75628
Neel Doshi <60827173+neeldoshii@users.noreply.github.com>
2022-07-15 05:07:39 +0530

    Patterns

--- patterns/hollowrectangle.cpp
+++ patterns/hollowrectangle.cpp
@@ -10,17 +10,24 @@ int main()
 
     cout<<"Enter the no of Columms you want?";
     cin>>columns;
-    
-    for (int i = 0; i <= rows; i++)
-    {
-        for (int j = 0; j <= columns; j++)
-        {
-        cout<<"*";
-        }
 
+    for (int i = 1; i <= rows; i++)
+    {
+        for (int j = 1; j <= columns; j++)
+        {
+            if (i==1 || i == rows || j == 1 || j == columns)
+            {
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+            
+        }
         cout<<endl;
-        
     }
     
+    
+
     return 0;
 }
