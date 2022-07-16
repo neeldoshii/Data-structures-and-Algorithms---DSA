902017f3009531b91dbeb6c7c9244a868d463f3a
Neel Doshi <60827173+neeldoshii@users.noreply.github.com>
2022-07-17 03:56:15 +0530

    armstrong number

---
+++ Basics/armstrong.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+  
+// Driver Code
+int main()
+{
+    //armstrong no mewna the all single digits cube's sum is equal to the number given
+
+    int num;
+    cout<<"Enter a number to check is it armstrong or not :";
+    cin>>num;
+    int ogno=num;
+    int lastdigit;
+    int digit;
+    int total = 0;
+
+    while (ogno>0)
+    {
+        lastdigit = ogno % 10;
+        lastdigit = lastdigit * lastdigit * lastdigit;
+        total+= lastdigit;
+        ogno = ogno/10;
+    }
+    // cout<<total<<" "<<num;
+    if (total == num)
+    {
+        cout<<"The number is armstrong number"<<endl;
+    }else{
+        cout<<"The number is not armstrong number"<<endl;
+    }
+    
+    
+
+    return 0;
+}
